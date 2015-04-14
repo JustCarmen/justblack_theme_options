7b644deba8e0b13ff5506560719fca929049b4d2
JustCarmen <carmen@justcarmen.nl>
2015-04-14 07:39:24 +0200

    Switch to the default wt function. It now has an option to set the necessary class.

--- module.php
+++ module.php
@@ -328,16 +328,6 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 		Log::addConfigurationLog($this->getTitle() . ' reset to default values');
 	}
 
-	// Radio buttons
-	private function radioButtons($name, $selected) {
-		$values = array(
-			0	 => I18N::translate('no'),
-			1	 => I18N::translate('yes'),
-		);
-
-		return radio_buttons($name, $values, $selected, 'class="radio-inline"');
-	}
-
 	private function config() {
 
 		if (Filter::postBool('save') && Filter::checkCsrf()) {
@@ -553,7 +543,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Use the Family tree title in the header?'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[TREETITLE]', $this->options('treetitle')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[TREETITLE]', $this->options('treetitle'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('Choose “no” if you have used the Family tree title in your custom header image. Otherwise leave value to “yes”.'); ?></p>
 								</div>
 							</div>
@@ -665,7 +655,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Resize image (800 x 150px)'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('resize', '0'); ?>
+									<?php echo edit_field_yes_no('resize', '0', 'class="radio-inline"'); ?>
 								</div>
 							</div>
 							<!-- HEADER HEIGHT -->
@@ -693,7 +683,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Use flags in header bar as language menu?'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[FLAGS]', $this->options('flags')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[FLAGS]', $this->options('flags'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('You can use flags in the bar above the topmenu bar in the header. These flags replaces the default dropdown menu. We advice you not to use this option if you have more then ten languages installed. You can remove unused languages from the folder languages in your webtrees installation.'); ?></p>
 								</div>
 							</div>
@@ -703,7 +693,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Use a compact menu?'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[COMPACT_MENU]', $this->options('compact_menu')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[COMPACT_MENU]', $this->options('compact_menu'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('In the compact “View”-menu the menus for Charts, Lists, Calendar and (optionally) Reports will be merged together.'); ?></p>
 								</div>
 							</div>
@@ -713,7 +703,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Include the reports topmenu in the compact \'View\' topmenu?'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[COMPACT_MENU_REPORTS]', $this->options('compact_menu_reports')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[COMPACT_MENU_REPORTS]', $this->options('compact_menu_reports'), 'class="radio-inline"'); ?>
 								</div>
 							</div>
 							<!-- MEDIA MENU -->
@@ -723,7 +713,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Media menu in topmenu'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[MEDIA_MENU]', $this->options('media_menu')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[MEDIA_MENU]', $this->options('media_menu'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('If this option is set the media menu will be moved to the topmenu.'); ?></p>
 									<?php if (count($folders) > 1): // add extra information about subfolders ?>
 									<p class="small text-muted"><?php echo I18N::translate('The names of first level media folders from your media folder on the server will be used as submenu items of the new media menu. Warning: these submenu items are not translated automatically. Use a custom language file to translate your menu items. Read the webrees WIKI for more information.'); ?></p>
@@ -747,7 +737,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Include subfolders'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[SHOW_SUBFOLDERS]', $this->options('show_subfolders')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[SHOW_SUBFOLDERS]', $this->options('show_subfolders'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('If you set this option the results on the media list page will include subfolders.'); ?></p>
 								</div>
 							</div>
@@ -758,7 +748,7 @@ class JustBlackThemeOptionsModule extends AbstractModule implements ModuleConfig
 									<?php echo I18N::translate('Use square thumbs'); ?>
 								</label>
 								<div class="col-sm-8">
-									<?php echo $this->radioButtons('NEW_JB_OPTIONS[SQUARE_THUMBS]', $this->options('square_thumbs')); ?>
+									<?php echo edit_field_yes_no('NEW_JB_OPTIONS[SQUARE_THUMBS]', $this->options('square_thumbs'), 'class="radio-inline"'); ?>
 									<p class="small text-muted"><?php echo I18N::translate('Set this option to “yes” to use square thumbnails in individual boxes and charts. If you choose “no” the default webtrees thumbnails will be used'); ?></p>
 								</div>
 							</div>
